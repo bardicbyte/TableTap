a50505d57868c775cd786ccfa4e0e249bd58b787
bardicbyte <alveeahnaf25@gmail.com>
2025-05-12 10:39:04 +1000

    added styling for the accounts pages

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('', include('django.contrib.auth.urls')),	
 ]   
 
