cc16264e38e85b454aa7a63d535df6a3aa2416c7
bardicbyte <alveeahnaf25@gmail.com>
2025-05-16 14:16:59 +1000

    Added CSRF_TRUSTED_ORIGINS to settings.py

--- app/tableTap/settings.py
+++ app/tableTap/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://infs3202-81f19561.uqcloud.net",
+]
